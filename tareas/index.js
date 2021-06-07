997a7bffdeaba25de7956fd44b10ae204a6a7824
comision9Formar <comision9formar@gmail.com>
2021-06-07 17:57:23 -0300

    agrega funciones modulo tarea

--- tareas/index.js
+++ tareas/index.js
@@ -1,5 +1,13 @@
 const fs = require('fs');
 
 module.exports = {
-    leerJSON : () => fs.readFileSync('./data/tareas.json','utf-8')
+    leerJSON : () => fs.readFileSync('./data/tareas.json','utf-8'),
+    guadarTarea : (titulo,estado) => {
+        let tareas = this.leerJSON()
+        let tarea = {
+            titulo,
+            estado
+        }
+        tareas.push(tarea)
+    }
 }
